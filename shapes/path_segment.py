82bd16714be2b168c0e68ee489e3832be6cf9106
CasperH <casperhofstede@gmail.com>
2024-11-15 10:11:33 +0100

    Finish box size is now a full node

--- shapes/path_segment.py
+++ shapes/path_segment.py
@@ -20,15 +20,35 @@ class PathSegment:
         self.body: None     # CAD swept body
 
     def adjust_start_and_endpoints(self, node_size, previous_end_point=None, next_start_point=None,
-                                   previous_curve_type=None, next_curve_type=None):
+                                previous_curve_type=None, next_curve_type=None):
 
         # Special handling for single-node end segments
         if len(self.nodes) == 1 and self.nodes[0].puzzle_end and previous_end_point is not None:
             # Create a new start node at the previous_end_point location
             start_node = Node(previous_end_point.x, previous_end_point.y, previous_end_point.z)
             start_node.segment_start = True
+
+            # **Move the puzzle end node by half the node size in the path direction**
+            end_node_point = cq.Vector(self.nodes[0].x, self.nodes[0].y, self.nodes[0].z)
+            entering_vector = end_node_point - previous_end_point
+
+            if entering_vector.Length == 0:
+                entering_vector = cq.Vector(1, 0, 0)  # Default direction
+
+            entering_direction = entering_vector.normalized()
+
+            move_distance = node_size / 2
+            move_vector = entering_direction * move_distance
+            adjusted_end = end_node_point + move_vector
+
+            # Update the puzzle end node's coordinates
+            self.nodes[0].x = adjusted_end.x
+            self.nodes[0].y = adjusted_end.y
+            self.nodes[0].z = adjusted_end.z
+            self.nodes[0].segment_end = True
+
+            # Insert the start node
             self.nodes.insert(0, start_node)
-            self.nodes[-1].segment_end = True
             return
 
         # Do not adjust end points in case of curves
@@ -64,6 +84,7 @@ class PathSegment:
             # Adjust end point
             end_node_point = cq.Vector(self.nodes[-1].x, self.nodes[-1].y, self.nodes[-1].z)
             prev_node_point = cq.Vector(self.nodes[-2].x, self.nodes[-2].y, self.nodes[-2].z)
+
             if next_start_point is not None:
                 exiting_vector = next_start_point - end_node_point
                 if exiting_vector.Length == 0:
@@ -76,15 +97,24 @@ class PathSegment:
 
             exiting_direction = exiting_vector.normalized()
 
-            if not self.nodes[-1].puzzle_end:
+            if self.nodes[-1].puzzle_end:
+                # **Move the puzzle end node by half the node size in the path direction**
+                move_distance = node_size / 2
+                move_vector = exiting_direction * move_distance
+                adjusted_end = end_node_point + move_vector
+
+                # Update the puzzle end node's coordinates
+                self.nodes[-1].x = adjusted_end.x
+                self.nodes[-1].y = adjusted_end.y
+                self.nodes[-1].z = adjusted_end.z
+                self.nodes[-1].segment_end = True  # Mark the last node as segment end
+            else:
                 # Adjust distance based on next segment's curve_type
                 adjust_distance = node_size if next_curve_type is not None else node_size / 2
                 adjusted_end = end_node_point + exiting_direction * adjust_distance
                 end_node = Node(adjusted_end.x, adjusted_end.y, adjusted_end.z)
                 end_node.segment_end = True
                 self.nodes.append(end_node)
-            else:
-                self.nodes[-1].segment_end = True  # Mark the last node as segment end
         else:
             # Handle segments with only one node (e.g., mounting nodes)
             node_point = cq.Vector(self.nodes[0].x, self.nodes[0].y, self.nodes[0].z)
@@ -115,24 +145,37 @@ class PathSegment:
 
             exiting_direction = exiting_vector.normalized()
 
-            if not self.nodes[0].puzzle_start and not self.nodes[0].puzzle_end:
-                # Adjust distances based on previous and next segments' curve_type
-                adjust_distance_start = node_size if previous_curve_type is not None else node_size / 2
-                adjust_distance_end = node_size if next_curve_type is not None else node_size / 2
+            if self.nodes[0].puzzle_end:
+                # **Move the puzzle end node by half the node size in the path direction**
+                move_distance = node_size / 2
+                move_vector = exiting_direction * move_distance
+                adjusted_end = node_point + move_vector
 
-                adjusted_start = node_point - entering_direction * adjust_distance_start
-                start_node = Node(adjusted_start.x, adjusted_start.y, adjusted_start.z)
-                start_node.segment_start = True
-
-                adjusted_end = node_point + exiting_direction * adjust_distance_end
-                end_node = Node(adjusted_end.x, adjusted_end.y, adjusted_end.z)
-                end_node.segment_end = True
-
-                self.nodes.insert(0, start_node)
-                self.nodes.append(end_node)
+                # Update the puzzle end node's coordinates
+                self.nodes[0].x = adjusted_end.x
+                self.nodes[0].y = adjusted_end.y
+                self.nodes[0].z = adjusted_end.z
+                self.nodes[0].segment_end = True
             else:
-                self.nodes[0].segment_start = self.nodes[0].puzzle_start
-                self.nodes[0].segment_end = self.nodes[0].puzzle_end
+                if not self.nodes[0].puzzle_start and not self.nodes[0].puzzle_end:
+                    # Adjust distances based on previous and next segments' curve_type
+                    adjust_distance_start = node_size if previous_curve_type is not None else node_size / 2
+                    adjust_distance_end = node_size if next_curve_type is not None else node_size / 2
+
+                    adjusted_start = node_point - entering_direction * adjust_distance_start
+                    start_node = Node(adjusted_start.x, adjusted_start.y, adjusted_start.z)
+                    start_node.segment_start = True
+
+                    adjusted_end = node_point + exiting_direction * adjust_distance_end
+                    end_node = Node(adjusted_end.x, adjusted_end.y, adjusted_end.z)
+                    end_node.segment_end = True
+
+                    self.nodes.insert(0, start_node)
+                    self.nodes.append(end_node)
+                else:
+                    self.nodes[0].segment_start = self.nodes[0].puzzle_start
+                    self.nodes[0].segment_end = self.nodes[0].puzzle_end
+
 
     def copy_attributes_from(self, other_segment):
         self.profile_type = other_segment.profile_type
