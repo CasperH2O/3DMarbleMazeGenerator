65a4a612c69a89b0c0f385b5886242cf7cbc6496
CasperH2O <casperhofstede@gmail.com>
2025-03-09 21:02:29 +0100

    Prototype for arc sweeps

---
+++ cad/prototypes/build123d/path_sweep_circular_nodes.py
@@ -0,0 +1,89 @@
+from build123d import *
+from ocp_vscode import *
+
+# U-shape profile parameters
+adjusted_width = adjusted_height = 10 - 0.0001
+wall_thickness = 1.2
+
+half_width = adjusted_width / 2
+half_height = adjusted_height / 2
+inner_half_width = half_width - wall_thickness
+inner_half_height = half_height - wall_thickness
+
+# Define the points of the U-shape
+profile_pts = (
+    (-half_width, half_height),
+    (-inner_half_width, half_height),
+    (-inner_half_width, -inner_half_height),
+    (inner_half_width, -inner_half_height),
+    (inner_half_width, half_height),
+    (half_width, half_height),
+    (half_width, -half_height),
+    (-half_width, -half_height),
+    (-half_width, half_height),  # Close back to the first point
+)
+
+# Points for the path
+path_pts_1 = [
+    (-10, -30, 0),
+    (-10, -36.14208, 0),
+    (-5, -36.8210, 0)
+]
+
+# Points for the path
+path_pts_2 = [
+    (-5, -36.8210, 0),
+    (0, -37.5, 0),
+    (0, -33.75, 00),
+]
+
+# Points for the path
+path_pts_3 = [
+    (0, 0, 0),
+    (0, 40, 0),
+    (-40, 20, 0),
+    #(0, -45, 0),
+    #(0, -45, 0),
+]
+
+
+'''
+# Perform the first sweep
+with BuildPart() as swept_shape_part_1:
+    with BuildLine() as path_line_1:
+        Polyline(path_pts_1)      
+    # Create the U-shape sketch in the YZ plane
+    with BuildSketch(path_line_1.line^0) as u_shape_sketch_1:
+        with BuildLine(Rot(Z=-90)) as u_shape_line_1:
+            Polyline(profile_pts)
+        make_face()
+    sweep(transition=Transition.RIGHT)    
+
+# Perform the second sweep
+with BuildPart() as swept_shape_part_2:
+    with BuildLine() as path_line_2:
+        Polyline(path_pts_2)      
+    # Create the U-shape sketch in the YZ plane
+    with BuildSketch(path_line_1.line^1) as u_shape_sketch_2:
+        with BuildLine(Rot(Z=-90)) as u_shape_line_2:
+            Polyline(profile_pts)
+        make_face()
+    sweep(transition=Transition.RIGHT)
+    '''
+
+# Perform the third sweep
+with BuildPart() as swept_shape_part_3:
+    with BuildLine() as path_line_3:
+        Polyline(path_pts_3)  
+        RadiusArc((-40, 20, 0), (-40, 0, 0), -40)
+    # Create the U-shape sketch in the YZ plane
+    with BuildSketch(path_line_3.line^0) as u_shape_sketch_3:
+        with BuildLine(Rot(Z=-90)) as u_shape_line_3:
+            Polyline(profile_pts)
+        make_face()
+    sweep(transition=Transition.RIGHT)
+
+# Display the results
+#set_defaults(reset_camera=Camera.KEEP)
+set_defaults(reset_camera=Camera.RESET)
+show_all()
